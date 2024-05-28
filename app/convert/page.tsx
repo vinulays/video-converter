f9f7aefd8855cf70f02dfa85bc86061a494fec1f
Vinula Senarathne <vinulapc@gmail.com>
2024-05-28 19:06:09 +0530

    feat: video file name and size

--- app/convert/page.tsx
+++ app/convert/page.tsx
@@ -1,13 +1,26 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 export default function Convert() {
   const inputRef = useRef<HTMLInputElement>(null);
+  const [selectedVideoFile, setSelectedVideoFile] = useState<File>();
+  const [videoFileSize, setVideoFileSize] = useState<Number>(0);
 
   const handleOuterDivClick = () => {
     if (inputRef.current) {
       inputRef.current.click();
+    }
+  };
+
+  const handleFileChange = (event: any) => {
+    const file = event.target.files[0];
+
+    if (file) {
+      setSelectedVideoFile(file);
+      const sizeInMB = file.size / 1_000_000;
+      console.log(`File size: ${sizeInMB.toFixed(2)} MB`);
+      setVideoFileSize(sizeInMB);
     }
   };
 
@@ -18,47 +31,75 @@ export default function Convert() {
           Convert media directly on your device with our server-free technology.
         </div>
       </div>
-      <div className="pt-10 container max-w-4xl lg:max-w-6xl 2xl:max-w-7xl">
-        <div
-          role="presentation"
-          className="bg-background h-72 lg:h-80 xl:h-96 rounded-3xl shadow-sm border-secondary border-2 border-dashed cursor-pointer flex items-center justify-center"
-          onClick={handleOuterDivClick}
-        >
-          <input
-            ref={inputRef}
-            accept="video/mp4,video/x-m4v,video/webm,video/*, .mkv"
-            tabIndex={-1}
-            type="file"
-            className="hidden"
-          />
-          <div className="space-y-4 text-foreground">
-            <div className="justify-center flex text-3xl  md:text-4xl lg:text-6xl">
+      {!selectedVideoFile && (
+        <div className="pt-10 container max-w-4xl lg:max-w-6xl 2xl:max-w-7xl">
+          <div
+            role="presentation"
+            className="bg-background h-72 lg:h-80 xl:h-96 rounded-3xl shadow-sm border-secondary border-2 border-dashed cursor-pointer flex items-center justify-center"
+            onClick={handleOuterDivClick}
+          >
+            <input
+              ref={inputRef}
+              onChange={handleFileChange}
+              accept="video/mp4,video/x-m4v,video/webm,video/*, .mkv"
+              tabIndex={-1}
+              type="file"
+              className="hidden"
+            />
+            <div className="space-y-4 text-foreground">
+              <div className="justify-center flex text-3xl  md:text-4xl lg:text-6xl">
+                <svg
+                  stroke="currentColor"
+                  fill="none"
+                  strokeWidth="2"
+                  viewBox="0 0 24 24"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <polyline points="16 16 12 12 8 16"></polyline>
+                  <line x1="12" y1="12" x2="12" y2="21"></line>
+                  <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
+                  <polyline points="16 16 12 12 8 16"></polyline>
+                </svg>
+              </div>
+              <h3 className="text-center font-medium text-lg md:text-2xl">
+                Select a video file
+              </h3>
+              <h5 className="text-center text-sm md:text-base text-gray-400">
+                Supports any video format, any size.
+              </h5>
+            </div>
+          </div>
+        </div>
+      )}
+
+      {/* Video file card */}
+      {selectedVideoFile && (
+        <div className="pt-10 container max-w-4xl lg:max-w-6xl 2xl:max-w-7xl">
+          <div className="bg-background h-24 rounded-3xl shadow-sm border-secondary border cursor-pointer flex items-center justify-center">
+            <span className="text-2xl text-orange-600">
               <svg
                 stroke="currentColor"
-                fill="none"
-                strokeWidth="2"
-                viewBox="0 0 24 24"
-                strokeLinecap="round"
-                strokeLinejoin="round"
+                fill="currentColor"
+                stroke-width="0"
+                viewBox="0 0 16 16"
                 height="1em"
                 width="1em"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <polyline points="16 16 12 12 8 16"></polyline>
-                <line x1="12" y1="12" x2="12" y2="21"></line>
-                <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
-                <polyline points="16 16 12 12 8 16"></polyline>
+                <path d="M.002 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2zm1 9v1a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V9.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062zm5-6.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0"></path>
               </svg>
-            </div>
-            <h3 className="text-center font-medium text-lg md:text-2xl">
-              Select a video file
-            </h3>
-            <h5 className="text-center text-sm md:text-base text-gray-400">
-              Supports any video format, any size.
-            </h5>
+            </span>
+            <span className="ml-3">{selectedVideoFile.name}</span>
+            <span className="text-gray-500 ml-1">
+              ({videoFileSize.toFixed(2)} MB)
+            </span>
           </div>
         </div>
-      </div>
+      )}
     </main>
   );
 }
