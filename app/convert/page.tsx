144bc099efa3889b86e82a74188523fad18079ca
Vinula Senarathne <vinulapc@gmail.com>
2024-05-30 17:29:42 +0530

    feat: delete selected video button

--- app/convert/page.tsx
+++ app/convert/page.tsx
@@ -13,6 +13,11 @@ export default function Convert() {
     }
   };
 
+  const resetFiles = () => {
+    setSelectedVideoFile(undefined);
+    setVideoFileSize(0);
+  };
+
   const handleFileChange = (event: any) => {
     const file = event.target.files[0];
 
@@ -79,24 +84,45 @@ export default function Convert() {
       {/* Video file card */}
       {selectedVideoFile && (
         <div className="pt-10 container max-w-4xl lg:max-w-6xl 2xl:max-w-7xl">
-          <div className="bg-background h-24 rounded-3xl shadow-sm border-secondary border cursor-pointer flex items-center justify-center">
-            <span className="text-2xl text-orange-600">
+          <div className="px-5 bg-background h-24 rounded-3xl shadow-sm border-secondary border cursor-pointer flex items-center justify-between">
+            <div className="flex">
+              <span className="text-2xl text-orange-600">
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  stroke-width="0"
+                  viewBox="0 0 16 16"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M.002 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2zm1 9v1a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V9.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062zm5-6.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0"></path>
+                </svg>
+              </span>
+              <span className="ml-3">{selectedVideoFile.name}</span>
+              <span className="text-gray-500 ml-1">
+                ({videoFileSize.toFixed(2)} MB)
+              </span>
+            </div>
+
+            {/* Close button */}
+            <div
+              onClick={resetFiles}
+              className="cursor-pointer hover:bg-gray-100 rounded-full h-10 w-10 flex items-center justify-center text-2xl text-foreground"
+            >
               <svg
                 stroke="currentColor"
                 fill="currentColor"
                 stroke-width="0"
-                viewBox="0 0 16 16"
+                viewBox="0 0 24 24"
                 height="1em"
                 width="1em"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path d="M.002 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2zm1 9v1a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V9.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062zm5-6.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0"></path>
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path d="M19 6.41 17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
               </svg>
-            </span>
-            <span className="ml-3">{selectedVideoFile.name}</span>
-            <span className="text-gray-500 ml-1">
-              ({videoFileSize.toFixed(2)} MB)
-            </span>
+            </div>
           </div>
         </div>
       )}
