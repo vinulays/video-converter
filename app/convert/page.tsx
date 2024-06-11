d7e384226a7fded472e619f80fc1ecf157f13e85
Vinula Senarathne <vinulapc@gmail.com>
2024-06-11 18:50:27 +0530

    fix: strokeWidth console error

--- app/convert/page.tsx
+++ app/convert/page.tsx
@@ -317,7 +317,7 @@ export default function Convert() {
                   <svg
                     stroke="currentColor"
                     fill="currentColor"
-                    stroke-width="0"
+                    strokeWidth="0"
                     viewBox="0 0 16 16"
                     height="1em"
                     width="1em"
